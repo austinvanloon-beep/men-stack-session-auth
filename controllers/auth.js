6d06f2919d90cf8cdf43cfc133848ce5d77da2a7
Austin Vanloon <wic404learn@gmail.com>
2025-07-22 10:43:05 -0600

    create user func complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user.js')
+const bcrypt = require('bcrypt');
+
 
 router.get('/sign-up', async (req, res) => {
     res.render('auth/sign-up.ejs')
 })
+
+router.post("/sign-up", async (req, res) => {
+const userInDataBase = await User.findOne({ username: req.body.username })
+
+if (userInDataBase) {
+    return res.send('Username already taken')
+}
+
+if (req.body.password !== req.body.confirmPassword) {
+    return rawListeners.send('Password and Confirm')
+}
+
+const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+req.body.password = hashedPassword
+
+const user = await User.create(req.body)
+
+  res.send(`Thanks for signing up ${user.username}`);
+});
 
 
 module.exports = router
